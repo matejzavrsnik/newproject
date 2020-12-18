57ee56d5fbd96e8f840bdda9486e0def3a692ee7
Matej Zavrsnik <matejzavrsnik@gmail.com>
2020-12-18 18:03:48 +0000

    import imgui backend files from submodule rather than drop-ins

--- library/src/lib.cpp
+++ library/src/lib.cpp
@@ -1,38 +1,12 @@
 #include "../include/library/header.h"
 
 #include <imgui.h>
-#include "imgui_impl_sdl.h"
-#include "imgui_impl_opengl3.h"
+#include <imgui_impl_sdl.h>
+#include <imgui_impl_opengl3.h>
+#include "imgui_opengl_loader_include.h" // needs to be before imgui_sdl_init.h and after imgui_impl_opengl3.h
+#include <imgui_stdlib.h> // extension to ImGui that supports some std library facilities like std::string
 #include <stdio.h>
 #include <SDL2/SDL.h>
-
-//#define IMGUI_IMPL_OPENGL_LOADER_GLEW
-
-// About Desktop OpenGL function loaders:
-//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
-//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
-//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
-#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-#include <GL/gl3w.h>            // Initialize with gl3wInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-#include <GL/glew.h>            // Initialize with glewInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-#include <glad/glad.h>          // Initialize with gladLoadGL()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
-#include <glad/gl.h>            // Initialize with gladLoadGL(...) or gladLoaderLoadGL()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
-#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
-#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
-#include <glbinding/gl/gl.h>
-using namespace gl;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
-#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
-#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
-#include <glbinding/gl/gl.h>
-using namespace gl;
-#else
-#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
-#endif
 
 #include "internal.h"
 
